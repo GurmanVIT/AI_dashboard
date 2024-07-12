503c7b688304c875213bb225f78abf5e06992c65
My Name <anmoldang121@gmail.com>
2024-07-13 00:16:29 +0530

    google signin

--- src/component/SignIn/SignIn.js
+++ src/component/SignIn/SignIn.js
@@ -1,56 +1,80 @@
-import React from 'react';
-import ai_img from '../../assets/img/ai_img.svg';
-import { Button } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
-import { Link, useNavigate } from 'react-router-dom';
+import React from "react";
+import ai_img from "../../assets/img/ai_img.svg";
+import { Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import { Link, useNavigate } from "react-router-dom";
 import google from "../../assets/img/google.svg";
 import facebook from "../../assets/img/facebook.svg";
+import { GoogleLogin } from "@react-oauth/google";
 
 const SignIn = () => {
+  const navigation = useNavigate();
 
-    const navigation = useNavigate();
+  const responseMessage = (response) => {
+    console.log(response);
+  };
+  const errorMessage = (error) => {
+    console.log(error);
+  };
 
-    return (
-        <>
-            <div className='inner'>
-                <div className='sec_bg'>
-                    <div className='logotype'>
-                        <img src={ai_img} alt='ai_img' />
-                        <h3>Logotype</h3>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
-                <div className='signup_bg'>
-                    <div className='text_btn'>
-                        <h4>Already have an account?</h4>
-                        <Button onClick={() => navigation(-1)}>Sign Up</Button>
-                    </div>
-                    <div className='signup_form'>
-                        <Form>
-                            <h3>Sign In</h3>
-                            <Form.Group className="mb-3 form_group" controlId="formBasicEmail">
-                                <Form.Label>Phone Number</Form.Label>
-                                <Form.Control type="number" placeholder="Enter Mobile Number" />
-                            </Form.Group>
-                            <p>Forgot Account?</p>
-                            <div className='next_btn'>
-                                <Button>Sign In</Button>
-                            </div>
-                            <div class="separator">
-                                <span>or</span>
-                            </div>
-                            <div className='goog_fac_btn'>
-                                <Link to="/" class="button facebook"><img src={google} alt='google' /> Google</Link>
-                                <Link to="/" class="button facebook"><img src={facebook} alt='facebook' /> Facebook</Link>
-                            </div>
-                        </Form>
-                    </div>
+  return (
+    <>
+      <div className="inner">
+        <div className="sec_bg">
+          <div className="logotype">
+            <img src={ai_img} alt="ai_img" />
+            <h3>Logotype</h3>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="signup_bg">
+          <div className="text_btn">
+            <h4>Already have an account?</h4>
+            <Button onClick={() => navigation(-1)}>Sign Up</Button>
+          </div>
+          <div className="signup_form">
+            <Form>
+              <h3>Sign In</h3>
+              <Form.Group
+                className="mb-3 form_group"
+                controlId="formBasicEmail"
+              >
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control type="number" placeholder="Enter Mobile Number" />
+              </Form.Group>
+              <p>Forgot Account?</p>
+              <div className="next_btn">
+                <Button>Sign In</Button>
+              </div>
+              <div class="separator">
+                <span>or</span>
+              </div>
+              <div className="goog_fac_btn">
+                <GoogleLogin
+                  onSuccess={responseMessage}
+                  onError={errorMessage}
+                />
+                {/* <Link to="/" class="button facebook">
+                  <img src={google} alt="google" /> Google
+                </Link> */}
+                <Link to="/" class="button facebook">
+                  <img src={facebook} alt="facebook" /> Facebook
+                </Link>
+              </div>
+            </Form>
+          </div>
 
-                    <p>Protected by reCAPTCHA and subject to the Rhombus <br /><b>Privacy Policy</b> and <b>Terms of Service.</b></p>
-                </div>
-            </div>
-        </>
-    )
-}
+          <p>
+            Protected by reCAPTCHA and subject to the Rhombus <br />
+            <b>Privacy Policy</b> and <b>Terms of Service.</b>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default SignIn;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="630737192676-470r9qhp8s5fauvg7d8apcpndpc432as.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
